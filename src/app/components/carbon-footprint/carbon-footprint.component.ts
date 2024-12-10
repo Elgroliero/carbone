a384ca890e37ff43fada93acc1886631d0052f5a
stropee <stropee@campus-eni.fr>
2024-12-10 11:04:47 +0100

    TP 3

--- src/app/components/carbon-footprint/carbon-footprint.component.ts
+++ src/app/components/carbon-footprint/carbon-footprint.component.ts
@@ -1,42 +1,66 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CarbonFootprintFormComponent} from "../carbon-footprint-form/carbon-footprint-form.component";
 import {CarbonFootprintResultComponent} from "../carbon-footprint-result/carbon-footprint-result.component";
+import {DecimalPipe, NgClass, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-carbon-footprint',
   standalone: true,
   imports: [
     CarbonFootprintFormComponent,
-    CarbonFootprintResultComponent
+    CarbonFootprintResultComponent,
+    NgStyle,
+    DecimalPipe,
+    NgClass
   ],
   templateUrl: './carbon-footprint.component.html',
   styleUrl: './carbon-footprint.component.css'
 })
 export class CarbonFootprintComponent {
+
+  public readonly maxConsumption: number = 7
+  public readonly minConsumption: number = 4
+  public distanceKm!: number
+  public consumptionFor100!: number
+  public travels: any[] = []
+
   ngOnInit() {
-    console.log('Le composant a été initialisé.');
+    this.distanceKm = 50;
+    this.consumptionFor100 = 5;
+
+    this.travels = [
+      {distanceKm: 50, consumptionFor100: 5},
+      {distanceKm: 150, consumptionFor100: 6},
+      {distanceKm: 250, consumptionFor100: 7},
+      {distanceKm: 350, consumptionFor100: 8},
+      {distanceKm: 450, consumptionFor100: 9}
+    ]
+
+    this.calculateDistanceAndAverage()
   }
 
-  ngOnDestroy() {
-    console.log('Le composant a été détruit.');
+  public add100Km() {
+    this.distanceKm += 100;
   }
 
-  ngAfterContentInit() {
-    console.log('Le contenu du composant a été initialisé.');
+  addTravel() {
+    const distance = Math.floor(Math.random() * 1000)
+    const consumption = Math.floor(Math.random() * 10)
+    this.travels.push({distanceKm: distance, consumptionFor100: consumption})
+    this.calculateDistanceAndAverage()
   }
 
-  ngAfterContentChecked() {
-    console.log('Le contenu du composant est vérifié.');
-  }
+  public calculateDistanceAndAverage() {
+    let totalDistance  = 0;
+    let average = 0;
 
-  ngAfterViewInit() {
-    console.log('La vue du composant a été initialisée.');
+    for(const travel of this.travels){
+      totalDistance += travel.distanceKm;
+      average += travel.consumptionFor100
+    }
+    this.distanceKm = totalDistance;
+    this.consumptionFor100 = average / this.travels.length
   }
-
-  ngAfterViewChecked() {
-    console.log('La vue du composant a été vérifiée.');
-  }
-
 
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  public username! : string
+
+  ngOnInit(): void {
+    this.username = 'Michel';
+  }
 
 }
